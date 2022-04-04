f9b209ed03a19beb652a22f250a646b7e5a08f74
NasseriYa <webme724@gmail.com>
2022-04-04 22:01:30 +0430

    insert data
    -insert new data into local storage
    -complete step 1 practice roocket

--- index.js
+++ index.js
@@ -1,40 +1,76 @@
-
-var reply_click = function()
-{
-    alert("Button clicked, id "+this.id+", text"+this.innerHTML);
+var reply_click = function () {
+    alert("Button clicked, id " + this.id + ", text" + this.innerHTML);
+}
+var showInsertDataItem = function () {
+    var x = document.getElementById("insertedItemDiv");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
 
-var callFunctionStart = function (){
+function insertNewDataItem() {
+
+    const price = document.getElementById('price').value;
+    const type = document.getElementById('type').value;
+    const desc = document.getElementById('desc').value;
+
+    if (price.length <= 1 || desc.length <= 1) {
+        alert('همه فیلد ها را پر نمایید');
+        return false;
+    }
+
+    const oldData = JSON.parse(localStorage.getItem("data_practice"));
+    let lastId = oldData.slice(-1).pop()
+
+    const arrayNewData = {"id": lastId.id+1, "price": price, "desc": desc, "type": type, "date": new Date()};
+    // oldData.push(JSON.stringify(arrayNewData));
+    oldData.push(arrayNewData);
+    localStorage.setItem("data_practice", JSON.stringify(oldData));
+    alert('اطلاعات به درستی ثبت شد');
+    location.reload();
+    // console.log(oldData);
+
+}
+
+var callFunctionStart = function () {
 
     const data = [
         {
             "id": 1,
             "price": 5000,
-            "desc": "موجودی اولیه صندوق",
-            "type": "واریز",
+            "desc": "واریزی توسط اقای ایمانی",
+            "type": 1,
             "date": new Date(),
         },
         {
             "id": 2,
             "price": 3000,
-            "desc": "واریز به صندوق توسط آقای ایمانی",
-            "type": "واریز",
+            "desc": "برداشت از صندوق توسط آقای ایمانی - خرید لامپ",
+            "type": 2,
             "date": new Date(),
         }
     ];
 
-    var storedNames = JSON.parse(localStorage.getItem("data_practise"));
+    var storedNames = JSON.parse(localStorage.getItem("data_practice"));
     // console.log(storedNames.length);
 
-    if(storedNames===null){
-        localStorage.setItem("data_practise", JSON.stringify(data));
+    if (storedNames === null) {
+        localStorage.setItem("data_practice", JSON.stringify(data));
         console.log('create data');
     }
     console.log('load data');
 
     document.getElementById('dataTable').innerHTML = storedNames.map((data, i) =>
         // '<li>' + d.id + ' '+d.price+' '+ d.desc+' '+d.type+' '+d.date+ '</li>'
-        '<tr><td>'+data.id+'</td><td>'+data.price+'</td><td>'+data.type+'</td><td>'+data.desc+'</td><td>'+data.date+'</td></tr>'
+        '<tr>' +
+        '<td>' + data.id + '</td>' +
+        '<td>' + data.price + '</td>' +
+        '<td>' + data.desc + '</td>' +
+        '<td>' + (data.type === 1 ? '<lable class="text-success">واریز</lable>' : '<lable class="text-danger">برداشت</lable>') + '</td>' +
+        '<td>' + data.date + '</td>' +
+        '</tr>'
     );
 
 }
@@ -42,4 +78,6 @@ var callFunctionStart = function (){
 
 window.onload = callFunctionStart;
 
-document.getElementById('testClick').onclick = reply_click;
+// document.getElementById('testClick').onclick = reply_click;
+document.getElementById('insertData').onclick = showInsertDataItem;
+document.getElementById('btnInserted').onclick = insertNewDataItem;
