354e963b644721967508998eb5800dbe7e4df010
naser zare <70854929+nsa14@users.noreply.github.com>
2022-04-05 13:29:05 +0430

    complete step 2
    -add fa font
    -add modal box
    -add sum of price
    -add persian date picker
    -add method delete
    -complete method insert

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-var reply_click = function () {
-    alert("Button clicked, id " + this.id + ", text" + this.innerHTML);
-}
-var showInsertDataItem = function () {
-    var x = document.getElementById("insertedItemDiv");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
+// var reply_click = function () {
+//     alert("Button clicked, id " + this.id + ", text" + this.innerHTML);
+// }
+//  function showInsertDataItem() {
+//     const x = document.getElementById("insertedItemDiv");
+//     if (x.style.display === "none") {
+//         x.style.display = "block";
+//     } else {
+//         x.style.display = "none";
+//     }
+// }
 
 function insertNewDataItem() {
-
     const price = document.getElementById('price').value;
     const type = document.getElementById('type').value;
     const desc = document.getElementById('desc').value;
+    const dateTime = document.getElementById('dateTime').value;
+    // const day = document.getElementById('day').value;
+    // const month = document.getElementById('month').value;
+    // const year = document.getElementById('year').value;
+    // const slashDate = year + '/' + month + '/' + day;
 
     if (price.length <= 1 || desc.length <= 1) {
         alert('همه فیلد ها را پر نمایید');
@@ -24,7 +28,13 @@ function insertNewDataItem() {
     const oldData = JSON.parse(localStorage.getItem("data_practice"));
     let lastId = oldData.slice(-1).pop()
 
-    const arrayNewData = {"id": lastId.id+1, "price": price, "desc": desc, "type": type, "date": new Date()};
+    const arrayNewData = {
+        "id": lastId.id + 1,
+        "price": parseInt(price),
+        "desc": desc,
+        "type": parseInt(type),
+        "date": dateTime
+    };
     // oldData.push(JSON.stringify(arrayNewData));
     oldData.push(arrayNewData);
     localStorage.setItem("data_practice", JSON.stringify(oldData));
@@ -34,8 +44,8 @@ function insertNewDataItem() {
 
 }
 
-var callFunctionStart = function () {
-
+ function callFunctionStart() {
+    // defined default data in load page.
     const data = [
         {
             "id": 1,
@@ -65,23 +75,64 @@ var callFunctionStart = function () {
     document.getElementById('dataTable').innerHTML = storedNames.map((data, i) =>
         // '<li>' + d.id + ' '+d.price+' '+ d.desc+' '+d.type+' '+d.date+ '</li>'
         '<tr>' +
-        '<td>' + data.id + '</td>' +
-        '<td>' + numberWithCommas(data.price) + '</td>' +
-        '<td>' + data.desc + '</td>' +
+        '<td>' + ++i + '</td>' +
+        '<td>' + numberWithCommas(data.price) + ' تومان</td>' +
         '<td>' + (data.type === 1 ? '<lable class="text-success">واریز</lable>' : '<lable class="text-danger">برداشت</lable>') + '</td>' +
         '<td>' + data.date + '</td>' +
+        '<td><button onclick="openModal(' + data.id + ')" class="btn btn-xs btn-primary">نمایش</button><button onclick="showDeleteConfirm(' + data.id + ');" class="btn btn-xs btn-danger">خذف</button></td>' +
         '</tr>'
     );
+
+    const depositType = storedNames.filter(function (el) {
+        return el.type === 1;
+    });
+    let totalSalaryDeposit = depositType.map(item => item.price).reduce((prev, curr) => prev + curr, 0);
+
+    const withdrawType = storedNames.filter(function (el) {
+        return el.type === 2;
+    });
+    let totalSalaryWithdraw = withdrawType.map(item => item.price).reduce((prev, curr) => prev + curr, 0);
+
+    document.getElementById('deposit').innerHTML = numberWithCommas(totalSalaryDeposit);
+    document.getElementById('withdraw').innerHTML = numberWithCommas(totalSalaryWithdraw);
 
 }
 
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
+function showDeleteConfirm(id){
+    document.getElementById('_id').value = id;
+    $("#modalConfirm").modal('show');
+}
+function deleteRow() {
+    const id = document.getElementById('_id').value;
+    const storedNames = JSON.parse(localStorage.getItem("data_practice"));
+    const newData = storedNames.filter(function (ele) {
+        return ele.id !== parseInt(id);
+    });
+    localStorage.setItem("data_practice", JSON.stringify(newData));
+    console.log(newData);
+    document.getElementById('_id').value = null;
+    // alert('به درستی حذف شد');
+    location.reload();
+}
+
+function openModal(id) {
+    const storedNames = JSON.parse(localStorage.getItem("data_practice"));
+    const newData = storedNames.filter(function (ele) {
+        return ele.id === id;
+    });
+
+    document.getElementById('Mprice').innerHTML=newData[0].price;
+    document.getElementById('Mdesc').innerHTML= newData[0].desc;
+    document.getElementById('Mtype').innerHTML= newData[0].type ===1 ? 'واریز' : 'برداشت';
+    document.getElementById('Mdate').innerHTML= newData[0].date;
+
+    $("#exampleModal").modal('show');
+}
 
 
 window.onload = callFunctionStart;
-
-// document.getElementById('testClick').onclick = reply_click;
-document.getElementById('insertData').onclick = showInsertDataItem;
 document.getElementById('btnInserted').onclick = insertNewDataItem;
+
