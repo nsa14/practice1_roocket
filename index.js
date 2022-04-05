57100549fccdcb70f096f348483b8d6ec7e905f6
NasseriYa <webme724@gmail.com>
2022-04-05 22:23:02 +0430

    complete step 3
    -add chart js plugin
    -add convert jalali to en plugin

--- index.js
+++ index.js
@@ -27,8 +27,6 @@ function insertNewDataItem() {
 
     const oldData = JSON.parse(localStorage.getItem("data_practice"));
     let lastId = oldData.slice(-1).pop()
-    const d = new Date();
-    const dateString = d.getFullYear() + "/" + d.getMonth() + "/" + d.getDate();
 
     const arrayNewData = {
         "id": lastId.id + 1,
@@ -36,7 +34,7 @@ function insertNewDataItem() {
         "desc": desc,
         "type": parseInt(type),
         "date": dateTime,
-        "dateEn": 'dateString'
+        "dateEn": jalaliToGregorian(dateTime, '/')
     };
     // oldData.push(JSON.stringify(arrayNewData));
     oldData.push(arrayNewData);
@@ -50,7 +48,7 @@ function insertNewDataItem() {
 function callFunctionStart() {
     // defined default data in load page.
     const d = new Date();
-    const dateString = d.getFullYear() + "/" + d.getMonth() + "/" + d.getDate();
+    let dateString = d.getFullYear() + "/" + d.getMonth() + "/" + d.getDate();
     const data = [
         {
             "id": 1,
@@ -58,15 +56,15 @@ function callFunctionStart() {
             "desc": "واریزی توسط اقای ایمانی",
             "type": 1,
             "date": '1401/01/16',
-            "dateEn": dateString,
+            "dateEn": jalaliToGregorian('1401/01/16', '/')
         },
         {
             "id": 2,
             "price": 3000,
             "desc": "برداشت از صندوق توسط آقای ایمانی - خرید لامپ",
             "type": 2,
-            "date": '1401/01/16',
-            "dateEn": dateString,
+            "date": '1400/10/2',
+            "dateEn": jalaliToGregorian('1400/10/2', '/')
         }
     ];
 
@@ -122,6 +120,14 @@ function callFunctionStart() {
 
 }
 
+function jalaliToGregorian(myDate, seperator){
+    var myDate = myDate,
+        dateSplitted = myDate.split(seperator),
+        jD = JalaliDate.jalaliToGregorian(dateSplitted[0], dateSplitted[1], dateSplitted[2]);
+    return  (jD[0] + seperator + jD[1] + seperator + jD[2]);
+    // console.log(jResult);
+}
+
 function numberWithCommas(x) {
     return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
@@ -159,7 +165,7 @@ function openModal(id) {
 }
 
 function createChartJs(depositNumber, withdrawNumber) {
-    var options = {
+    let options = {
         exportEnabled: true,
         animationEnabled: true,
         title: {
@@ -172,14 +178,14 @@ function createChartJs(depositNumber, withdrawNumber) {
             title: "بر اساس ماه"
         },
         axisY: {
-            title: "Units Sold",
+            title: "واریز",
             titleFontColor: "#4F81BC",
             lineColor: "#4F81BC",
             labelFontColor: "#4F81BC",
             tickColor: "#4F81BC"
         },
         axisY2: {
-            title: "Profit in USD",
+            title: "برداشت",
             titleFontColor: "#C0504E",
             lineColor: "#C0504E",
             labelFontColor: "#C0504E",
@@ -193,7 +199,7 @@ function createChartJs(depositNumber, withdrawNumber) {
             itemclick: toggleDataSeries
         },
         data: [{
-            type: "spline",
+            type: "line",
             name: "واریز",
             showInLegend: true,
             xValueFormatString: "MMM YYYY",
@@ -208,12 +214,13 @@ function createChartJs(depositNumber, withdrawNumber) {
             // ]
         },
             {
-                type: "spline",
+                type: "line",
                 name: "برداشت",
                 axisYType: "secondary",
                 showInLegend: true,
                 xValueFormatString: "MMM YYYY",
                 yValueFormatString: "$#,##0.#",
+                lineDashType: "dash",
                 dataPoints: withdrawNumber
                 // dataPoints: [
                 //     {x: new Date(2016, 0, 1), y: 19034.5},
@@ -235,6 +242,60 @@ function createChartJs(depositNumber, withdrawNumber) {
 
 }
 
+JalaliDate = {
+    g_days_in_month: [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31],
+    j_days_in_month: [31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 29]
+};
+JalaliDate.jalaliToGregorian = function(j_y, j_m, j_d) {
+    j_y = parseInt(j_y);
+    j_m = parseInt(j_m);
+    j_d = parseInt(j_d);
+    var jy = j_y - 979;
+    var jm = j_m - 1;
+    var jd = j_d - 1;
+
+    var j_day_no = 365 * jy + parseInt(jy / 33) * 8 + parseInt((jy % 33 + 3) / 4);
+    for (var i = 0; i < jm; ++i) j_day_no += JalaliDate.j_days_in_month[i];
+
+    j_day_no += jd;
+
+    var g_day_no = j_day_no + 79;
+
+    var gy = 1600 + 400 * parseInt(g_day_no / 146097); /* 146097 = 365*400 + 400/4 - 400/100 + 400/400 */
+    g_day_no = g_day_no % 146097;
+
+    var leap = true;
+    if (g_day_no >= 36525) /* 36525 = 365*100 + 100/4 */
+    {
+        g_day_no--;
+        gy += 100 * parseInt(g_day_no / 36524); /* 36524 = 365*100 + 100/4 - 100/100 */
+        g_day_no = g_day_no % 36524;
+
+        if (g_day_no >= 365) g_day_no++;
+        else leap = false;
+    }
+
+    gy += 4 * parseInt(g_day_no / 1461); /* 1461 = 365*4 + 4/4 */
+    g_day_no %= 1461;
+
+    if (g_day_no >= 366) {
+        leap = false;
+
+        g_day_no--;
+        gy += parseInt(g_day_no / 365);
+        g_day_no = g_day_no % 365;
+    }
+
+    for (var i = 0; g_day_no >= JalaliDate.g_days_in_month[i] + (i == 1 && leap); i++)
+        g_day_no -= JalaliDate.g_days_in_month[i] + (i == 1 && leap);
+    var gm = i + 1;
+    var gd = g_day_no + 1;
+
+    gm = gm < 10 ? "0" + gm : gm;
+    gd = gd < 10 ? "0" + gd : gd;
+
+    return [gy, gm, gd];
+}
 
 window.onload = callFunctionStart;
 document.getElementById('btnInserted').onclick = insertNewDataItem;
